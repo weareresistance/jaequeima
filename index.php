ca8a4abbd15aa8c119578c294aab434156ef76f1
TiagoAryan <tiago_aryan@hotmail.com>
2016-04-27 16:50:16 +0100

    v2.2 online

--- index.php
+++ index.php
@@ -3,19 +3,59 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Já é Queima?</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable = no" />
+		<meta name="description" content="Ja é Queima das Fitas em Coimbra?">
+		<meta name="author" content="RESISTANCE">
+		<meta name="keywords" content="">
+		<meta name="robots" content="index, follow">
+		<meta name="googlebot" content="index, follow">
+		
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicon/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicon/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicon/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicon/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicon/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicon/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="assets/img/favicon/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="assets/img/favicon/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="assets/img/favicon/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="assets/img/favicon/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="assets/img/favicon/manifest.json">
+		<link rel="mask-icon" href="assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="apple-mobile-web-app-title" content="Ja é Queima?">
+		<meta name="application-name" content="Ja é Queima?">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="assets/img/favicon/mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
 
-		<?php $incioQueima = "2016-05-04 21:00:00"; ?>
+		<?php $incioQueima = "2016-05-04 21:00:00"; 
+		    
+    $today = date('Y-m-d');
+    $today = strtotime($today);
+    $finish = strtotime($incioQueima);
+    //difference
+    $diff = $finish - $today;
+
+    $daysleft=floor($diff/(60*60*24));
+?>
+		
 
 		<?php if (new DateTime() > new DateTime($incioQueima)) { ?>
         	<link rel="stylesheet" href="assets/css/jaequeima_dias.css">
 		<?php } else { ?>
 			<link rel="stylesheet" href="assets/css/jaequeima.css">
-			<meta property="og:url"           content="http://www.your-domain.com/your-page.html" />
-			<meta property="og:type"          content="website" />
-			<meta property="og:title"         content="Já é Queima" />
-			<meta property="og:description"   content="Your description" />
-			<meta property="og:image"         content="http://localhost:8888/jaequeima/img/background.png" />
 		<?php } ?>
+		
+		
+		<meta property="og:url"           content="http://jaequeima.pt" />
+		<meta property="og:type"          content="website" />
+		<meta property="og:title"         content="Ja é Queima?" />
+		<meta property="og:description"   content="Ja é Queima das Fitas em Coimbra ?" />
+		<meta property="og:image"         content="http://jaequeima.pt/assets/img/queima-facebook.jpg" />
 
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Oswald:300,400,700"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
@@ -131,7 +171,7 @@
 				<div class="container_bottom">
 					<img id="logo" src="assets/img/logo_big.png">
 					<div class="button_fb" id="share_button" >
-		                <a href="https://www.facebook.com/sharer/sharer.php?u=localhost:8888/jaequeima/jaequeima.php" target="_blank">
+		                <a href="https://www.facebook.com/sharer/sharer.php?u=http://jaequeima.pt/" target="_blank">
 		                    <i class="fa fa-facebook-official"></i> Partilhar
 		                </a>
 		            </div>
